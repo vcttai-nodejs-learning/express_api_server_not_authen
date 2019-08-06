ed95a3ebbe08a6fcc9318c206a95114775dd8c84
tai-vu <tai-vu@cybozu.vn>
2019-08-06 18:10:29 +0700

    API: Get one, Delete one, Edit one

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Subscriber = require('../models/subscriber');
+const { getSubscriber } = require ('../middleware/subscriber_mdw');
 
 router.get('/', async (req, res) => {
     try {
@@ -12,18 +13,18 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', getSubscriber, (req, res) => {
+    res.json(res.subscriber);
 })
 
 router.post('/', async (req, res) => {
-    const subcriber = new Subscriber({
+    const subscriber = new Subscriber({
         name: req.body.name,
         subscribedChannel: req.body.subscribedChannel
     });
 
     try {
-        const newSubscriber = await subcriber.save();
+        const newSubscriber = await subscriber.save();
         res.status(201).json(newSubscriber);
     }
     catch (err) {
@@ -31,12 +32,32 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if (req.body.name != null) {
+        res.subscriber.name = req.body.name;
+    }
 
+    if (req.body.subscribedChannel != null) {
+        res.subscriber.subscribedChannel = req.body.subscribedChannel;
+    }
+
+    try {
+        const updatedSubscriber = await res.subscriber.save();
+        res.json(updatedSubscriber);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 })
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+        await res.subscriber.remove();
+        res.json({ message: 'Deleted this subscriber!'});
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 })
 
 module.exports = router;
